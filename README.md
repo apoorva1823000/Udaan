75e703ee69183166ef427bd03a0a874d3e9ef163
Apoorva .S. Mehta <71769587+apoorva1823000@users.noreply.github.com>
2023-01-29 16:31:12 +0530

    Files added

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# Udaan
-Rewarding crowdfunding system for Ingenium hackathon
+# Udaan - The Rewarding CrowdFundraiser
+Udaan is a crowdfunding smart contract with a twist. It is a smart contract built on Ethereum in solidity language that can be readily deployed on any existing blockchain supporting Ethereum like Shardeum, Polygon etc. The change we added to the age old crowd funding systems is a way to incentivize the projects that help in environmental educations as well as reducing carbon footprint and hence helping in managing the climate crisis. Udaan – A project by LoopBreakers aims to support every noble cause by providing a platform to fund the cause but additionally also educate people to work towards a greener Earth by prioritizing the causes/ startup projects that work towards the similar goal
+
+Features include:
+- Decentralised System
+- Trustless crowd fundraiser
+- Rewarding to the contributors
+- Supportive to nature friendly startups
+- Deployable on polygon, ethereum, sharedum
+
+## Why is the need
+
+Globally, the average carbon footprint is closer to 4 tons. To have the best chance of avoiding a 2℃ rise in global temperatures, the average global carbon footprint per year needs to drop to under 2 tons by 2050. Scientists first began to worry about climate change toward the end of the 1950s and has been since an existing segment to work upon. Also as of 2022, a total of 80,152 startups had been officially recognized by DPIIT. Though crowdfunding isn’t limited to funding a startup but when it comes to funding for such a cause, why not to filter out those that also support a healthier environment for the future generation especially when doing so could educate people to come up with better and smarter ideas  
+
+## How we aim to support ?
+Team LoopBreakers aims at providing a solution to the existing problem mentioned afore by proposing a decentralised smart contract to raise a requirement for crowd funding and if the cause is noble in the way that it could potentially be of less carbon footprint then you may exclusively get an NFT as a reward towards your contribution. Blockchain can help us achieve a better as well as stable solution to track funding processes as well as deploy a smarter mechanism to reward the environment rangers that aim at working for the Earth.
+
+## Examples
+
+- Educational needs
+- Medical needs
+- Smart cleaner bots (green startups)
+
+## Future work
+
+We're working on some other features for making development here a lot easier, which are included, but not limited to:
+
+- Providing authenticity
+- Making full fledged flutter application
+- Rewarding with something more than just NFTs
+
+## Feedback
+
+Please leave any comments on this repl's [spotlight page here](https://replit.com/@replit/Solidity-starter-beta?v=1).
+
+The UI is subject to change (and still needs some real responsive work), so we'd appreciate any feedback there. 
+
+We're also trying to improve the overall dev experience, especially for beginners, so any feedback there is appreciated. 
---
+++ contract.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.5.0 < 0.9.0;
+contract CrowdFunding{
+
+    event CarbonEmitted(address from, uint amount);
+    event CarbonOffset(address from, uint amount);
+
+
+    mapping(address=>uint) public contributors;
+    address public manager;
+    uint public minimumContribution;
+    uint public deadline;
+    uint public target;
+    uint public raisedAmount;
+    uint public noOfContributors;
+
+    mapping (address => uint) public carbonFootprint;
+    uint public totalCarbonEmitted;
+    uint public totalCarbonOffset;
+
+    struct Request{
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address=>bool) voters;
+    }
+
+    mapping(uint=>Request)public requests;
+    uint public numRequests;
+
+    constructor(uint _target,uint _deadline){
+        target = _target;
+        deadline = block.timestamp+_deadline;
+        minimumContribution = 5 wei;
+        manager = msg.sender;
+        
+    }
+    function sendEth() public payable {
+        require(block.timestamp < deadline, "Deadline has passed");
+        require(msg.value >= minimumContribution, "Minimum contribution is not met");
+        if(contributors[msg.sender]==0){
+            noOfContributors++;
+        }
+        contributors[msg.sender]+=msg.value;
+        raisedAmount+=msg.value;
+    }
+    
+    function getContractBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function refund() public{
+        require(block.timestamp > deadline && raisedAmount<target,"You are not eligible for refund");
+        require(contributors[msg.sender]>0);
+        address payable user = payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender]=0;
+    }
+
+    modifier onlyManager(){
+        require(msg.sender==manager,"Only manager can call this function");
+        _;
+    }
+
+    function createRequests(string memory _description, address payable _recipient, uint _value) public onlyManager{
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value; 
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
+    } 
+
+    function voteRequest(uint _requestNo) public{
+        require(contributors[msg.sender]>0,"You must be a contributor");
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.voters[msg.sender]==false,"You have already voted");
+        thisRequest.voters[msg.sender]=true;
+        thisRequest.noOfVoters++;
+    }
+
+    function makePayment(uint _requestNo) public onlyManager{
+        require(raisedAmount>=target);
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.completed==false,"The requirement for this request has met");
+        require(thisRequest.noOfVoters> noOfContributors/2,"Unfortunately the majority does not support");
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed=true;
+
+    }
+
+    function emitCarbon(uint amount) public {
+        require(amount > 0);
+        carbonFootprint[msg.sender] += amount;
+        totalCarbonEmitted += amount;
+        emit CarbonEmitted(msg.sender, amount);
+    }
+
+    function offsetCarbon(uint amount) public {
+        require(amount > 0);
+        require(carbonFootprint[msg.sender] >= amount);
+        carbonFootprint[msg.sender] -= amount;
+        totalCarbonOffset += amount;
+        emit CarbonOffset(msg.sender, amount);
+    }
+
+    function getCarbonFootprint() public view returns (uint) {
+        return carbonFootprint[msg.sender];
+    }
+
+    function getTotalCarbonEmitted() public view returns (uint) {
+        return totalCarbonEmitted;
+    }
+
+    function getTotalCarbonOffset() public view returns (uint) {
+        return totalCarbonOffset;
+    }
+}
